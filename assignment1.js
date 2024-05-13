0b6efc1809293833c4f89175d758b8d37a94a6ce
emadchy96 <echowdhury7@myseneca.ca>
2024-05-13 16:35:25 -0400

    Step 2
    Code Added. Functional. No bugs.

---
+++ assignment1.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+* WEB700 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Emad Chowdhury Student ID: 161788237 Date: 13 May 2024
+*
+********************************************************************************/
+
+// Step 1
+console.log('Hello World!');
